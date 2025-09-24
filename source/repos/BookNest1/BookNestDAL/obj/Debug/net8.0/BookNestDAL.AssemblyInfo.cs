b1321c6ed66629967a56eadac989ef25dba57595
Manjunath Naik <raddymanju317@gmail.com>
2025-09-24 19:38:23 +0530

    First commit after clone

--- source/repos/BookNest1/BookNestDAL/obj/Debug/net8.0/BookNestDAL.AssemblyInfo.cs
+++ source/repos/BookNest1/BookNestDAL/obj/Debug/net8.0/BookNestDAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookNestDAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f253e78f2ea32b4b3a36ffb9294686da36d78f94")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookNestDAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookNestDAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
